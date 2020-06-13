aae64ae2a37662c24e325e05cbb1d78ee7c6fd1c
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-06-13 12:30:41 +0200

    Feat: Mapear entidad RetroGames

--- src/main/java/org/elsmancs/practica/domain/RetroGame.java
+++ src/main/java/org/elsmancs/practica/domain/RetroGame.java
@@ -1,5 +1,38 @@
 package org.elsmancs.practica.domain;
 
-public class RetroGame {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "t_items")
+public class RetroGame {
+	
+	@Id
+	@Column(name = "item_nom")
+	private String nom;
+	
+	@Column(name = "item_prop")
+	private int dificultad;
+	
+	@Column(name = "item_tipo")
+	private String tipo;
+
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return nom;
+	}
+
+	public int getDificultad() {
+		// TODO Auto-generated method stub
+		return dificultad;
+	}
+
+	public String getTipo() {
+		// TODO Auto-generated method stub
+		return tipo;
+	}
+	
+	
 }
